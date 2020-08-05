2cddc3edddab539f100b492b7ced81ea24fbbc60
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-08-05 02:55:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def count_sentences
-  sentence_array = self.split(/[.?!]/)
-  return sentence_array.count
+    sentence_array = self.split(/[.?!]/)
+    return sentence_array.count
   end
+  
 end
